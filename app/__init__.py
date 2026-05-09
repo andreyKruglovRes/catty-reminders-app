652b3ef886452aee9076904d2028e894d5d1140c
Andrey Kruglov <andrey@example.com>
2026-05-09 05:19:24 -0400

    6313 Лаб.1: Добавлен автоматический deployref

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ This module builds shared parts for other modules.
 
 import json
 import os
+import subprocess
 
 from fastapi.templating import Jinja2Templates
 
@@ -21,7 +22,16 @@ with open('config.json') as config_json:
   users = config['users']
   db_path = config['db_path']
 
-DEPLOY_REF = os.getenv("DEPLOY_REF", "NA")
+def get_git_hash():
+    try:
+        return subprocess.check_output(
+            ['git', 'rev-parse', '--short', 'HEAD'], 
+            cwd='/home/kali/catty-reminders-app'
+        ).decode('ascii').strip()
+    except Exception:
+        return "NA"
+
+DEPLOY_REF = get_git_hash()
 
 # --------------------------------------------------------------------------------
 # Establish the Secret Key
